069200bff54427aac4f925782d88d063069423a2
georgiy <ewws@mail.ru>
2019-12-30 15:31:59 +0900

    changed somthing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React,{useState}from 'react';
 import img1 from './img/img1.jpg'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`
+const Button = styled.div`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+  font-size: 20px;
+  background-color: red;
+
+`
+const NewButton= styled(Button)`
+  background-color: blue;
+`
+
 const App=()=> {
   const [key,setKey]=useState(false)
+  const [testText,setTestText]=useState('')
   const a ='hello egor'
   const b ='hello gosha'
+  const addName = ()=>{
+    axios.post('http://localhost:1337/students', {
+      "name": testText
+    })
+
+  }
   const onClick = ()=>{
     axios.get('http://localhost:1337/answers')
       .then(function (data) {
@@ -22,12 +51,19 @@ const App=()=> {
   const secondFunc = ()=>{
     setKey(!key)
   }
+  const textOnChange = (arg)=>{
+    console.log(arg.target.value)
+    setTestText(arg.target.value);
+  }
+
   return (
-    <div>
+    <Container>
       {false?a:b}
-      <button onClick={onClick}>knopka</button>
+      <Button onClick={onClick}>knopka</Button>
+      <NewButton onClick={addName}>dobavit' egora</NewButton>
       {key?<img src={img1}/>:null}
-    </div>
+      <input  onChange={textOnChange}></input>
+    </Container>
   );
 }
 
